bddc021ea3e3f0267cd3a19057eac5997f705cd4
MirYasa <gurevich.miron14@gmail.com>
2022-02-07 00:16:01 +0300

    delete js files

--- src/constants/urls.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.CMC_BASE = exports.CMC_MAIN = void 0;
-exports.CMC_MAIN = 'https://coinmarketcap.com/?page=';
-exports.CMC_BASE = 'https://coinmarketcap.com';
