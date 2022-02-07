1ddafc8cf526331ac05e4fc5a65aceb8547add56
MirYasa <gurevich.miron14@gmail.com>
2022-02-07 22:07:28 +0300

    add parse data for currency

--- src/utils/getSpecificcrypto.ts
+++ src/utils/getSpecificcrypto.ts
@@ -11,8 +11,21 @@ export async function getSpecificcrypto(address: string) {
 
     const $ = cheerio.load(content)
 
-    $('div.top-summary-container').slice(0,1).each((i: number, elem: Element) => {
-        console.log($(elem).text())
-        // console.log($(elem).html())
+    // $('div.statsContainer div.statsBlockInner div.statsLabel, div.statsItemRight div.statsValue').each((i: number, elem: Element) => {
+    //     // console.log($(elem).text())
+    //     console.log($(elem).text())
+    // })
+    //
+    // $('ul.content a.link-button').each((i: number, elem: Element) => {
+    //     console.log($(elem).text(), $(elem).attr('href'))
+    // })
+
+    // $('div.priceValue, div.sliderSection span').each((i: number, elem: Element) => {
+    //     console.log($(elem).text())
+    // })
+    $('div.nameHeader').each((i: number, elem: Element) => {
+        $(elem).children().each((is, ds) => {
+            console.log($(ds).text())
+        })
     })
 }
